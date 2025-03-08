7ea6d2b66d1a794aed08d3881de129493651ba03
RaginiGoswami <raginigoswami000@gmail.com>
2025-03-08 13:02:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//  let mode = document.querySelector("#com");
+
+// let crtmode = "light";
+
+// mode.addEventListener("click", () => {
+// if (crtmode === "light"){
+//     crtmode="dark";
+// }
+// else {
+//     crtmode ="light";
+// }
+// console.log(crtmode);
+// });
+
+let divya = document.querySelector("#divya");
+
+let crtmode = "light";
+
+divya.addEventListener("click",()=>{
+if (crtmode=== "light"){
+    crtmode="dark";
+    document.body.style.backgroundColor="black";
+}
+else {
+    crtmode ="light";
+    document.body.style.backgroundColor="pink";
+
+}
+console.log(crtmode);
+});
